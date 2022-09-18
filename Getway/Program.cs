3d5bd8b3ea8d615fa33cbd459600dc7b1d9ce1d2
Erfan Shayegh <iefiwm@gmail.com>
2022-09-18 16:11:14 +0430

    update

--- Getway/Program.cs
+++ Getway/Program.cs
@@ -1,18 +1,10 @@
-using MMLib.SwaggerForOcelot.DependencyInjection;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Provider.Polly;
-using Ubitex.Travel.Base;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var routes = "Routes";
-
-builder.Configuration.AddOcelotWithSwaggerSupport(options =>
-{
-    options.Folder = routes;
-});
-
 builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
 {
     config
@@ -24,8 +16,6 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 
-//builder.Services.AddSwaggerGen();
-
 var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
     .AddOcelot(routes, builder.Environment)
@@ -34,8 +24,6 @@ builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
 //builder.Services.AddOcelot();
 
 builder.Services.AddOcelot(builder.Configuration).AddPolly();
-
-//builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
 builder.Services.AddCors(options => options.AddPolicy("CorsPolicy",
      builder =>
@@ -49,36 +37,14 @@ builder.Services.AddCors(options => options.AddPolicy("CorsPolicy",
 
 var app = builder.Build();
 
-//app.UseSwagger();
-
-//app.UseSwaggerUI(c =>
-//{
-//    c.SwaggerEndpoint("/client/swagger/v1/swagger.json", "Client");
-//    c.SwaggerEndpoint("/hotel/swagger/v1/swagger.json", "Hotel");
-//    c.SwaggerEndpoint("/flight/swagger/v1/swagger.json", "Flight");
-//    //c.RoutePrefix = "docs";
-//});
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
-//app.UseSwaggerForOcelotUI(options =>
-//{
-//    options.PathToSwaggerGenerator = "/swagger/docs";
-//    options.ReConfigureUpstreamSwaggerJson = AlterUpstream.AlterUpstreamSwaggerJson;
-//});
 
 await app.UseOcelot();
-
-//app.UseSwaggerForOcelotUI(options =>
-//{
-//    options.PathToSwaggerGenerator = "/swagger/docs";
-//    options.ReConfigureUpstreamSwaggerJson = AlterUpstream.AlterUpstreamSwaggerJson;
-
-//}).UseOcelot().Wait();
 
 app.UseCors("CorsPolicy");
 
